b43c64db589c5a5ab8265e36f85200848a65a3ed
Karson1531252 <1531252@learn.vsb.bc.ca>
2025-10-08 21:56:44 +0000

    assignemtn

--- 2.4/main.py
+++ 2.4/main.py
@@ -1,14 +1,3 @@
-file = open("2.4/responses.csv")
-
-for line in file :
-    if "karson" in line.lower():
-        print(line)
-        
-        if "Dog" in line:
-            print(line)
-
-
-    
 
 
 

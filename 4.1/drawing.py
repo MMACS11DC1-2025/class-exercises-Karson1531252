12f7534de53a073bdacc4cf6ac1143310c72c469
Karson1531252 <1531252@learn.vsb.bc.ca>
2025-10-15 22:01:22 +0000

    circle

--- 4.1/drawing.py
+++ 4.1/drawing.py
@@ -4,3 +4,27 @@ Explore the turtle drawing package to create an interactive drawing.
 It should use a while loop.
 Your program should also include at least one function you’ve made yourself 
 """
+
+import turtle
+smeg = turtle.Turtle()
+
+while True:
+    command = input("What shall I do? ")
+
+    if command == "stop":
+        break
+
+    if command == "l":
+        smeg.left(45)
+        smeg.forward(50)
+
+    if command == "r":
+        smeg.right(45)
+        smeg.forward(50)
+
+    if command == "f":
+        smeg.forward(50)
+
+    if command == "c":
+        smeg.circle(67)
+        

55a933b9c9f9988299da17827182f072917cf65c
Karson1531252 <1531252@learn.vsb.bc.ca>
2025-10-31 21:23:07 +0000

    projet

--- 4.4/projecy.py
+++ 4.4/projecy.py
@@ -1,3 +1,4 @@
+
 import turtle
 
 turtle = turtle.Turtle()
@@ -6,33 +7,30 @@ def drawTree(level, branchLength):
   
   
   if level > 0:
+    turtle.color("red")
     turtle.forward(branchLength)
-    turtle.left(72)
+    turtle.left(120)
     drawTree(level-1, branchLength/1.7)
     
-    turtle.left(72)
+    turtle.left(120)
     drawTree(level-1, branchLength/1.7)
     
-    turtle.left(72)
-    drawTree(level-1, branchLength/1.7)
-    
-    turtle.left(72)
-    drawTree(level-1, branchLength/1.7)
-    
-    turtle.left(72)
+    turtle.left(120)
     drawTree(level-1, branchLength/1.7)
     turtle.back(branchLength)
   
+  if level > 1:
+    turtle.color("blue")
+    
+    
 turtle.speed(0)
 turtle.penup()
 turtle.goto(0, -180)
 turtle.left(90)
 turtle.pendown()
 
-turtle.color("brown")
+turtle.color("red")
 turtle.width(3)
 turtle.shape("triangle")
 levels = input("How many levels do you want me to draw? ")
 drawTree(int(levels), 120)
-
-#colour

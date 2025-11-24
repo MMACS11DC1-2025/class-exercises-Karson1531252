985c68611eef65972f693ef4f72a7fbecb644ad9
Karson1531252 <1531252@learn.vsb.bc.ca>
2025-11-24 20:16:05 +0000

    jbeans

--- 5.1/jelly_beans.py
+++ 5.1/jelly_beans.py
@@ -1,24 +1,50 @@
-from PIL import Image\
+from PIL import Image
 
-def colour(r, g, b):
+file = Image.open("jelly_bean.png")
+jbimg = file.load()
+
+yellow_count = 0
+blue_count = 0
+green_count = 0
+red_count = 0
+pink_count = 0
+brown_count = 0
+purple_count = 0
+black_count = 0
+
+def is_green(r, g, b):
+    if r>=  0 and r < 25 and g <= 255 and b >= 0 and b < 25:
+        return True
+    else:
+        return False
+def is_yellow(r, g, b):
+    if r>=  0 and r < 25 and g <= 255 and b >= 0 and b < 25:
+        return True
+    else:
+        return False
+def is_green(r, g, b):
+    if r>=  0 and r < 25 and g <= 255 and b >= 0 and b < 25:
+        return True
+    else:
+        return False
+def is_green(r, g, b):
+    if r>=  0 and r < 25 and g <= 255 and b >= 0 and b < 25:
+        return True
+    else:
+        return False
+def is_green(r, g, b):
     if r>=  0 and r < 25 and g <= 255 and b >= 0 and b < 25:
         return True
     else:
         return False
 
-file = Image.open("5.1/jelly_beans.jpg").load()
-image_output = Image.open("5.1/jelly_beans.jpg")
+width = file.width
+height = file.height
 
-width = image_output.width
-height = image_output.height
+for x in range(width):
+    for y in range(height):
+        r = jbimg[x, y][0]
+        g = jbimg[x, y][1]
+        b = jbimg[x, y][2]
 
-for i in range (width):
-    for j in range(height):
-        r = file[i,j][0]
-        g = file[i,j][1]
-        b = file[i,j][2]
 
-        if jelly_beans(r,g,b):
-            image_output.putpixel((i,j), (255, 255, 255))
-
-image_output.save("5.1/output_jelly.jpg", "jpg")

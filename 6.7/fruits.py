9da2b449fdf33d15a03ce29b63000e1b0e4e09ee
Karson1531252 <1531252@learn.vsb.bc.ca>
2025-12-01 05:23:45 +0000

    fruits

--- 6.7/fruits.py
+++ 6.7/fruits.py
@@ -1,7 +1,16 @@
 from PIL import Image
 
 # List of fruit images
-fruit_images = ["6.7/apple.jpg", "6.7/banana.jpg", "6.7/orange.jpg", "6.7/grape.jpg", "6.7/lemon.jpg", "6.7/tomato.jpg", "6.7/mango.jpg", "6.7/blueberry.jpg", "6.7/watermelon.jpg", "6.7/kiwi,jpg"]
+fruit_images = ["6.7/apple.jpg",
+                "6.7/banana.jpg",
+                "6.7/orange.jpg",
+                "6.7/grape.jpg",
+                "6.7/lemon.jpg",
+                "6.7/tomato.jpg",
+                "6.7/mango.jpg",
+                "6.7/blueberry.jpg",
+                "6.7/watermelon.jpg",
+                "6.7/kiwi.jpg"]
 
 # Function to calculate average color
 def average_color(image):
@@ -27,6 +36,30 @@ for file in fruit_images:
     avg = average_color(img)
     results.append({"name": file, "rgb": avg})
 
-# Print results
+# --------- DAY 2 ADDITION ---------
+# Function to get user color
+def get_user_color():
+    color = input("Enter a color (red, green, yellow, orange, purple): ").lower()
+    return color
+
+# Function to return target RGB for basic colors
+def target_color(name):
+    colors = {
+        "red": (255, 0, 0),
+        "green": (0, 255, 0),
+        "yellow": (255, 255, 0),
+        "orange": (255, 165, 0),
+        "purple": (128, 0, 128)
+    }
+    return colors.get(name, (0, 0, 0))  # default black if unknown
+
+# Ask the user for a color
+user_color = get_user_color()
+target_rgb = target_color(user_color)
+
+print("\nTarget RGB for color", user_color.upper(), "is:", target_rgb)
+
+# Print the analyzed fruit averages again for reference
+print("\nFruit average colors:")
 for r in results:
     print(r)

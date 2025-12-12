f8c44a8c23132c85f76886c3bd7ad2c2a4560fee
Karson1531252 <1531252@learn.vsb.bc.ca>
2025-12-12 21:35:03 +0000

    j

--- 6.7/fruits.py
+++ 6.7/fruits.py
@@ -1,4 +1,5 @@
 from PIL import Image
+#use colour picker
 
 # List of fruit images
 fruit_images = ["6.7/apple.jpg",
@@ -12,22 +13,24 @@ fruit_images = ["6.7/apple.jpg",
                 "6.7/watermelon.jpg",
                 "6.7/kiwi.jpg"]
 
+usercolour = input("What colour do you like? Red, orange , yellow, green, blue or purple?")
 def colours(r, g, b):
-    if 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+    if 230 < r <= 255 and 0 <= g < 50 and 0 <= b < 50:
         return "red"
     
-    elif 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+    elif 230 < r <= 255 and 50 <= g <= 150 and 0 <= b < 50:
         return "orange"
     
     elif 230 < r <= 255 and 230 < g <= 255 and 0 <= b < 25:
         return "yellow"
     
-    elif 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+    elif 0 <= r < 25 and 230 < g < 255 and 0 <= b < 25:
         return "green"
     
-    elif 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+    elif 0 <= r < 50 and 0 <= g < 50 and 230 < b <= 255:
         return "blue"
     
-    elif 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+    elif 230 < r <= 255 and 0 <= g < 50 and 230 < b <= 255:
         return "purple"
-print(usercolour, red)
+
+

4f48f87e631c4718942ae7f727328a63865fc82b
Karson1531252 <1531252@learn.vsb.bc.ca>
2025-12-12 21:05:26 +0000

    j

--- 6.7/fruits.py
+++ 6.7/fruits.py
@@ -12,53 +12,22 @@ fruit_images = ["6.7/apple.jpg",
                 "6.7/watermelon.jpg",
                 "6.7/kiwi.jpg"]
 
-# Function to calculate average color
-def average_color(image):
-    pixels = list(image.getdata())
-    total_r = total_g = total_b = 0
-    count = len(pixels)
+def colours(r, g, b):
+    if 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+        return "red"
     
-    for pixel in pixels:
-        r, g, b = pixel[:3]
-        total_r += r
-        total_g += g
-        total_b += b
-
-    avg_r = total_r // count
-    avg_g = total_g // count
-    avg_b = total_b // count
-    return (avg_r, avg_g, avg_b)
-
-# Analyze all images
-results = []
-for file in fruit_images:
-    img = Image.open(file)
-    avg = average_color(img)
-    results.append({"name": file, "rgb": avg})
+    elif 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+        return "orange"
     
-# Function to get user color
-def get_user_color():
-    color = input("Enter a color (red, green, yellow, orange, purple): ").lower()
-    return color
-
-# Function to return target RGB for basic colors
-def target_color(name):
-    colors = {
-        "red": (255, 0, 0),
-        "green": (0, 255, 0),
-        "yellow": (255, 255, 0),
-        "orange": (255, 165, 0),
-        "purple": (128, 0, 128)
-    }
-    return colors.get(name, (0, 0, 0))  # default black if unknown
-
-# Ask the user for a color
-user_color = get_user_color()
-target_rgb = target_color(user_color)
-
-print("\nTarget RGB for color", user_color.upper(), "is:", target_rgb)
-
-# Print the analyzed fruit averages again for reference
-print("\nFruit average colors:")
-for r in results:
-    print(r)
+    elif 230 < r <= 255 and 230 < g <= 255 and 0 <= b < 25:
+        return "yellow"
+    
+    elif 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+        return "green"
+    
+    elif 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+        return "blue"
+    
+    elif 230 < r <= 255 and 0 <= g < 25 and 0 <= b < 25:
+        return "purple"
+print(usercolour, red)
